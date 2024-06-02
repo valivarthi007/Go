c9b1941b77fda5427621fe07cfb30a03b8f41e27
diwakar <diwakar.valivarthi@gmail.com>
2024-06-02 03:15:28 +0000

    updated main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@
 provider "aws" {
   region = "us-east-1"  
 }
-
 # Create IAM role for EKS cluster
 resource "aws_iam_role" "eks_cluster_role" {
   name               = "eks-cluster-role"
@@ -21,14 +20,11 @@ resource "aws_iam_role" "eks_cluster_role" {
 }
 EOF
 }
-
 # Attach IAM policies to the EKS cluster role
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
-  count      = length(aws_iam_role.eks_cluster_role) == 0 ? 1 : 0
-  role       = aws_iam_role.eks_cluster_role[0].name
+  role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
-
 # Create IAM role for EKS nodes
 resource "aws_iam_role" "eks_node_role" {
   name               = "eks-node-role"
@@ -47,20 +43,16 @@ resource "aws_iam_role" "eks_node_role" {
 }
 EOF
 }
-
 # Attach IAM policies to the EKS node role
 resource "aws_iam_role_policy_attachment" "eks_node_policy_attachment" {
-  count      = length(aws_iam_role.eks_node_role) == 0 ? 1 : 0
-  role       = aws_iam_role.eks_node_role[0].name
+  role       = aws_iam_role.eks_node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
-
 # Create security group for EKS cluster
 resource "aws_security_group" "eks_cluster_sg" {
-  count        = length(aws_security_group.eks_cluster_sg) == 0 ? 1 : 0
-  name         = "eks-cluster-sg"
-  description  = "Security group for EKS cluster"
-  vpc_id       = "vpc-0621b188e7dc1b0da"  
+  name        = "eks-cluster-sg"
+  description = "Security group for EKS cluster"
+  vpc_id      = "vpc-0621b188e7dc1b0da"  
   ingress {
     from_port   = 443
     to_port     = 443
@@ -74,26 +66,22 @@ resource "aws_security_group" "eks_cluster_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 # Create EKS cluster
 resource "aws_eks_cluster" "my_cluster" {
-  count       = length(aws_eks_cluster.my_cluster) == 0 ? 1 : 0
   name        = "my-cluster"
-  role_arn    = aws_iam_role.eks_cluster_role[0].arn
+  role_arn    = aws_iam_role.eks_cluster_role.arn
   version     = "1.21"
   
   vpc_config {
     subnet_ids         = ["subnet-01b32a3aaa42658c3", "subnet-06b9233cf6cb96132"]  # Replace with your subnet IDs
-    security_group_ids = [aws_security_group.eks_cluster_sg[0].id]
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]
   }
 }
-
 # Create EKS node group
 resource "aws_eks_node_group" "my_node_group" {
-  count             = length(aws_eks_node_group.my_node_group) == 0 ? 1 : 0
-  cluster_name      = aws_eks_cluster.my_cluster[0].name
-  node_group_name   = "my-node-group"
-  node_role_arn     = aws_iam_role.eks_node_role[0].arn
+  cluster_name    = aws_eks_cluster.my_cluster.name
+  node_group_name = "my-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
   
   scaling_config {
     desired_size = 3
